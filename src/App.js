0a9d24a90674d24409954606faf381539e8d6ee4
Matt Ruder <matt.j.ruder@gmail.com>
2023-05-31 12:00:01 -0500

    Add basic setup with image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Navbar'
+import Image from 'react-bootstrap/Image'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import headshot from './images/nicoheadshot.png'
 
 function App() {
 
@@ -11,8 +13,12 @@ const height = {
 
   return (
     <div  className="App">
+      <div className="nav-area">
      <NavBar />
-     
+     </div>
+     <div className="content">
+     <Image src={headshot} fluid />
+     </div>
     </div>
   );
 }
